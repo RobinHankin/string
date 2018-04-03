c7dc03b84d77f11759b60b42d7c2d6ec0449e54b
RobinHankin <hankin.robin@gmail.com>
2018-04-03 18:08:08 +1200

    add taut string

--- flamm.R
+++ flamm.R
@@ -6,15 +6,21 @@ pdf(file="flamm.pdf")
 library(plot3D)
 source("usefulfuncs.R")
 
+## values of r and theta to plot: use longer vectors to make the plot
+## look better but this takes longer
+
+thetavals <- seq(from=0,to=2*pi,len=100)
+rvals <-  seq(from=1,to=10,len=30)
+
+
 scatter3D(1:2,1:2,1:2,bty=NULL,type='n',lwd=1,pch=NA,
-          phi=50,r=1000,box=TRUE,scale=FALSE,
+          phi=60,r=1000,box=FALSE,scale=FALSE,
           xlim=c(-10,10),ylim=c(-10,10),zlim=c(-32,0),colkey=FALSE
           )
 
 
 par(mfrow = c(1, 1))
-thetavals <- seq(from=0,to=2*pi,len=10)
-rvals <-  seq(from=1,to=10,len=30)
+
 for(r in rvals){
                   
     x <- r*cos(thetavals)
@@ -35,13 +41,13 @@ for(theta in thetavals){
 
 ## Now draw a string
 
-dist_from_hole <- 3
+dist_from_hole <- 1.4
 pars <- c(eel=1) # dummy
 xy <-
     stringpoints(
         y_start = dist_from_hole,
         initial_string_angle = 0,
-        theta = seq(from=0,to=pi*0.2,len=100)
+        theta = seq(from=0,to=pi*0.67,len=100)
     )
 
 r <- sqrt(rowSums(xy^2))
@@ -49,6 +55,9 @@ x <- xy[,1]
 y <- xy[,2]
 z <- -4*sqrt(r-1)
 
+x <- c(rev(x),x)
+y <- c(-rev(y),y)
+z <- c(rev(z),z)
 lines3D(x, y, z,  bty = NULL, colkey=FALSE, 
         ticktype = "detailed", lwd = 5,add=TRUE,col='black')
 
