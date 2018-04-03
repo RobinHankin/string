1162efe9abde2c6fabb6712e932bd52b1667e009
RobinHankin <hankin.robin@gmail.com>
2018-04-03 17:18:15 +1200

    start string plotting part

--- flamm.R
+++ flamm.R
@@ -1,7 +1,11 @@
 ## plots Flamm's paraboloid; quite slow to plot on the screen, but fast to make a pdf.
 
 rm(list=ls())
+
+pdf(file="flamm.pdf")
 library(plot3D)
+source("usefulfuncs.R")
+
 scatter3D(1:2,1:2,1:2,bty=NULL,type='n',lwd=1,pch=NA,
           phi=50,r=1000,box=TRUE,scale=FALSE,
           xlim=c(-10,10),ylim=c(-10,10),zlim=c(-32,0),colkey=FALSE
@@ -28,3 +32,25 @@ for(theta in thetavals){
             ticktype = "detailed", lwd = 0.1,add=TRUE,col='red')
 }
 
+
+## Now draw a string
+
+dist_from_hole <- 3
+pars <- c(eel=1) # dummy
+xy <-
+    stringpoints(
+        y_start = dist_from_hole,
+        initial_string_angle = 0,
+        theta = seq(from=0,to=pi*0.2,len=100)
+    )
+
+r <- sqrt(rowSums(xy^2))
+x <- xy[,1]
+y <- xy[,2]
+z <- -4*sqrt(r-1)
+
+lines3D(x, y, z,  bty = NULL, colkey=FALSE, 
+        ticktype = "detailed", lwd = 5,add=TRUE,col='black')
+
+
+dev.off()
