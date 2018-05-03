36e5b9a471a9fb02837bff982b79f95727076668
RobinHankin <hankin.robin@gmail.com>
2018-05-04 11:15:07 +1200

    change the thing so the lines are above the 3oclock direction rather than below it

--- one_free_end_r_equals_2.R
+++ one_free_end_r_equals_2.R
@@ -57,20 +57,6 @@ theta_end2[cont(start_angles, c(1.56,1.57))] <-  pi/120
 theta_end2[cont(start_angles, c(1.57,1.58))] <-  pi/600
 theta_end2[cont(start_angles, c(1.58,1.59))] <-  pi/30000
 
-
-for(i in seq_along(start_angles)){
-    if(FALSE){
-  ## First the upwards strings:
-  xy <-
-    stringpoints(
-        y_start = dist_from_hole,
-        initial_string_angle = start_angles[i],
-        theta = seq(from=theta_start,to=theta_end1[i],len=100)
-    )
-
-        points(xy,type='l',col=cols[i],lwd=1)
-    }
-}
 ## now the downward ones:
 for(i in seq_along(start_angles)){
   xy <-
@@ -80,11 +66,12 @@ for(i in seq_along(start_angles)){
         theta = -seq(from=theta_start,to=theta_end2[i],len=1000)
     )
   rsq <- c(1+sum(xy[1,]^2),rowSums(xy^2)) # rsq == r-squared
-  inward <- diff(rsq)<0           # TRUE if the string is moving inward
+  inward <- diff(rsq)<0      # TRUE if the string is moving inward
 
   xy_inward  <- xy[ inward,]
   xy_outward <- xy[!inward,]
 
+  xy_inward[,2] <- -xy_inward[,2] 
   points(xy_inward ,type='l',col=cols[i],lwd=1)
   if(FALSE){  points(xy_outward,type='l',col=cols[i],lwd=0)}
 
@@ -97,4 +84,4 @@ for(i in seq_along(start_angles)){
 polargrid(rlab=1.8)
 segments(1,0,2,0,col='red')  # radial string
 points(dist_from_hole,0,pch=16)
-event_horizon(fill=FALSE)
+event_horizon(fill=TRUE)
