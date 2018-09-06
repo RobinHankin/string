5e9cfddcbd0c7aa7bc1155f80851e8cf5fd40881
RobinHankin <hankin.robin@gmail.com>
2018-09-07 10:10:24 +1200

    tidyup and use cutoffmatrix for ease of modification

--- closest_approach.R
+++ closest_approach.R
@@ -19,17 +19,45 @@ plot(NULL,asp=1,xlim=c(-jj,jj),ylim=c(-jj,jj),type='l',axes=FALSE,,xlab='',ylab=
 polargrid(rlab=6.5)
 ## setup ends
 
-dist <- exp(seq(from=log(1.01),to=log(6.5),len=40))
+
+dist <- dseq(from=1.076109317, to=7.5, len=100, power=2)
 
 thetamax <- 0.1 + dist*0  # default value, (almost) always do-able
 
+cutoffmatrix <- matrix(c(
+ 1.060, pi  ,
+ 1.076, 3.1 ,
+ 1.077, 3.1 ,
+ 1.078, 3.1 ,
+ 1.079, 3.1 ,
+ 1.080, 3.0 ,
+ 1.090, 3.0 ,
+ 1.10, 2.90 ,
+ 1.11, 2.87 ,
+ 1.13, 2.79 ,
+ 1.15, 2.71 ,
+ 1.17, 2.61 ,
+ 1.19, 2.51 ,
+ 1.21, 2.45 ,
+ 1.25, 2.42 ,
+ 1.31, 2.37 ,
+ 1.32, 2.30 ,
+ 1.33, 2.30 ,
+ 1.35, 2.30 ,
+ 1.41, 2.20 ,
+ 1.51, 2.10 ,
+ 1.70, 2.00 ,
+ 1.80, 1.90 ,
+ 1.90, 1.90 ,
+ 2.00, 1.90 ,
+ 2.10, 1.90 ,
+ 2.20, 1.60 ,
+ 2.40, 1.60 ,
+ 5.00, 1.60 ,
+ 9.00, 1.60 ),
+ ncol=2,byrow=TRUE)
 
-cuts <- c(        1,   1.06,  1.07, 1.08,   1.11,    1.13,    1.15,     1.17,  1.19,    1.21,    1.31,   1.41,  1.51, 1.7, 2.01,  5.00)
-vals <- c(  NA,      pi,    pi,   3.1,   2.87,   2.79,    2.71,     2.61,   2.51,  2.4,      2.4,     2.2,   2.1,  2.0, 1.9,    1.6,     1.0)
-f <- fun(cuts,vals)
-
-
-## Thus f(1.03) = pi, f(1.12) = 2.5
+f <- fun(cutoffmatrix[,1],cutoffmatrix[,2])
 
 for(i in seq_along(dist)){
   tseq <- seq(from=0,to=f(dist[i]),len=100)
