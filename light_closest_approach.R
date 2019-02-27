09c4569c54aba0bfafcc779aa074c068a77946f6
RobinHankin <hankin.robin@gmail.com>
2019-02-27 15:52:30 +1300

    more tweaks for visual effect

--- light_closest_approach.R
+++ light_closest_approach.R
@@ -19,8 +19,8 @@ source("usefulfuncs.R") # defines polargrid() etc
 source("usefullightfuncs.R") # defines lightpoints()
 
 r_start <- 2 # starting radius for light ray
-mask <- FALSE    # set to FALSE to see entire geodesic (TRUE is time-consuming but needed for publication quality)
-size_of_plot <- 3
+mask <- TRUE    # set to FALSE to see entire geodesic (TRUE is time-consuming but needed for publication quality)
+size_of_plot <- 6
 
 ## plot setup:
 par(xpd=TRUE)
@@ -49,11 +49,47 @@ plot(NULL,asp=1,xlim=jj,ylim=jj,type='l',axes=FALSE,xlab='',ylab='')
 ## free end in space).
 
 cutoffmatrix  <- matrix(c(
-    1,pi/2,
-    1.5,pi/2 + 0.1,
-    1.6,pi/2 + 2.0,
-    1.7,pi/2 + 1,
-    3.0,pi/2 + 0.3,
+    0,0.001 ,
+    0.01,0.2,
+    0.02,0.3,
+    0.05,0.4,
+    0.07,0.5,
+    0.08,0.5,
+    0.1,0.7,
+    0.2,1.0,
+    0.5,1.8,
+    0.7,2.2,
+    0.8,2.5,
+    0.9,2.8,
+
+    1,3,
+    1.1, pi+0.2,
+    
+    1.2,pi + 0.6,
+    1.3,pi + 1.1,
+
+    1.35,pi + 1.4,
+    1.40,pi + pi/2+0.3,
+    1.41,pi + pi/2+0.3,
+    1.42,pi+2.2,
+    1.43,pi+2.3,
+    1.44,pi+2.4,
+    1.47,pi+pi          ,
+    1.48,pi+pi,
+    1.5,2*pi,
+    1.505,2*pi+0.1,
+    1.51,6,
+    1.52,5.1,
+    1.53,5,
+    1.54,4.7,
+    1.55,4.6,
+    1.57,4,
+    1.6,3.9,
+    1.7,pi/2 + 1.1,
+    1.9,pi/2 + 0.9,
+    2.0,pi/2 + 0.7,
+    2.5,pi/2 + 0.6,
+    3.0,pi/2 + 0.1,
     3.7,pi/4,
     4.001,pi/6
     )
@@ -61,14 +97,16 @@ cutoffmatrix  <- matrix(c(
 
 colnames(cutoffmatrix) <- c("cuts","vals")
 fmax <- fun(cutoffmatrix[,1],cutoffmatrix[,2])
+fmax <- approxfun(cutoffmatrix[,1],cutoffmatrix[,2])
 
 
 r_start <- seq(from=0.5,to=4,len=200)
+r_start <- seq(from=0.01,to=4,len=70)
 
 for(r in r_start){
 
-  final_phi <- fmax(r)
-  print(final_phi)
+  final_phi <- pmin(fmax(r),2*pi)
+  print(r)
   xy1 <-
     stringu(
         r_start=r,  # r=3/2 is a circular (but unstable) orbit
@@ -96,8 +134,8 @@ circ <- function(r,...){
   th <- seq(from=0,to=2*pi,len=300)
   points(r*cos(th),r*sin(th),type="l",...)
 }
-circ(1.5,lwd=3,lty=2)   # ergosphere
-circ(4.0,lwd=3,lty=2)   # cut-off for pretty printing
+circ(1.5,lwd=3,lty=1,col="red")   # ergosphere
+#circ(4.0,lwd=3,lty=2)   # cut-off for pretty printing
 
 
 event_horizon(fill=FALSE)
