3bd4509dfc0829efa6c7ebe0a07e4a8a372b436f
RobinHankin <hankin.robin@gmail.com>
2019-06-20 08:32:40 +1200

    tidyup and dramatic pause

--- light_start_at_radius_of_sun.R
+++ light_start_at_radius_of_sun.R
@@ -6,7 +6,14 @@
 
 ## More documentation is given in light_starts_at_r_equals_2.R
 
-source("usefullightfuncs.R") # defines lightpoints()
+cat(paste("Einstein's prediction: ",4*G*M/sol^2/radius_of_sun*180/pi*60^2,"\n"))
+cat(paste("Eddington's measurement: ",1.75,"\n"))
+## calculated value printed at the end
+cat("calculating...\n")
+Sys.sleep(10)  # dramatic pause (the actual calculation is almost instantaneous)
+
+
+source("usefullightfuncs.R") # defines nullgeodesic()
 
 G <- 6.67408e-11  # Gravitational constant (SI)
 M <- 1.989e30     # mass of sun (SI)
@@ -15,7 +22,7 @@ radius_of_sun <- 695510e3 # (m)
 r_schwarz <- 2*G*M/sol^2 # Schwarzschild radius of sun (m)
 r_start  <- radius_of_sun/r_schwarz # starting radius for light ray
 
-initialangletotangent <- 0  # tangential
+initialangletotangent <- 0  # tangential ray
  
 small <- 0.1
 
@@ -32,6 +39,27 @@ light <-
 
 jj <- tail(light,2)
 
-deflection <- -2*as.vector(diff(jj[,1]) / diff(jj[,2])) # radians (NB double)
+angle <- as.vector(diff(jj[,1]) / diff(jj[,2]))  # radians
 
-print(deflection*180/pi*60^2)  # arcseconds; compare Einstein's value of 1.75
+deflection <- -2*angle
+
+## In the above, note multiplication by [minus] two because the
+## astronomical measurement---which we are trying to predict---is of
+## total deflection of distant starlight.  The total deflection is
+## comprised of deflection in the ingoing leg *plus* deflection on the
+## outgoing leg.  Function nullgeodesic() gives deflection on the
+## outgoing leg, as the light starts at (r=radius_of_sun, phi=0) [that
+## is, at the 3 o'clock position, at the radius of the sun, moving
+## parallel to the y-axis].  Variable 'angle' is the angle that the
+## light beam makes from the y-axis, at a large distance [one day I'll
+## get round to deriving this properly] from the sun.  Ingoing
+## deflection and outgoing deflection are equal by symmetry, and we
+## need to add the two deflections, which is why we have a factor of 2
+## here.
+
+
+
+arcseconds <- deflection*180/pi*60^2
+
+
+cat(paste("direct numerical integration:", arcseconds, "\n"))
