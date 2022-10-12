4ac1e094004b4647a263eeb086cb3ce89c85487a
Robin Hankin <hankin.robin@gmail.com>
2022-10-12 13:07:16 +1300

    slight improvements

--- heart.R
+++ heart.R
@@ -1,10 +1,9 @@
 
-n <- 130
-d <- 0.1
-D <- 1.4
+n <- 3430
+d <- pi/(30.125)
 theta <- rep(0,n)
 theta[1] <- 0    # start point
-theta[2] <- 2.4  # start point
+theta[2] <- pi  # start point
 
 for(i in 3:n){
            if(i%%4==2){  # starts here
@@ -20,16 +19,19 @@ for(i in 3:n){
     }
 }
 
-plot(c(-1,1),c(-1,1),asp=1,type="n")
-for(i in seq(from=1,to=146)){
+plot(c(-1,1),c(-1,1),asp=1,type="n",axes=FALSE,xlab='',ylab='')
+o <- seq(from=0,to=2*pi,len=100)
+# points(cos(o),sin(o),type="l",col="lightgray")
+
+for(i in seq(from=1,to=n)){
     segments(
         x0=cos(theta[i  ]),
         y0=sin(theta[i  ]),
         x1=cos(theta[i+1]),
         y1=sin(theta[i+1])
     )
+#    Sys.sleep(0.01)
 }
 
 
-o <- seq(from=0,to=2*pi,len=100)
-points(cos(o),sin(o),type="l",col="lightgray")
+
